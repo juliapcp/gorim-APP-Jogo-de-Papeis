df6488eb0e687a993ff48b3c1a8b717d818b458a
juliapcp <juliapcp2003@gmail.com>
2020-09-20 19:40:39 -0300

    criação de rotas para api

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const aplicacao = express();
 const rotas = require('./rotas');
+
+aplicacao.use(express.json());
 aplicacao.use(rotas);
 aplicacao.listen(3333);
--- backend/src/rotas.js
+++ backend/src/rotas.js
@@ -1,10 +1,50 @@
 const express = require('express');
-
+const request = require('request');
 const rotas = express.Router();
 
-rotas.post('/users', (requisicao, resposta) => {
-    const body = requisicao.body;
-    console.log(body);
+rotas.get('/empresarios', function (requisicao, resposta, next) {
+    request({
+        uri: 'http://localhost:8080/request/api/mestre/empresarios'
+    }).pipe(resposta);
+});
+
+rotas.get('/agricultores', function (requisicao, resposta, next) {
+    request({
+        uri: 'http://localhost:8080/request/api/mestre/agricultores'
+    }).pipe(resposta);
+});
+
+rotas.get('/infoMundo', function (requisicao, resposta, next) {
+    request({
+        uri: 'http://localhost:8080/request/api/mestre/infoMundo'
+    }).pipe(resposta);
+});
+
+rotas.get('/agricultor/:id', async (requisicao, resposta) => {
+    request({
+        uri: 'http://localhost:8080/request/api/agricultor/' + requisicao.params.id
+    }).pipe(resposta);
+});
+
+rotas.get('/empresario/:id', async (requisicao, resposta) => {
+    request({
+        uri: 'http://localhost:8080/request/api/empresario/' + requisicao.params.id
+    }).pipe(resposta);
+});
+
+rotas.get('/arquivoResumo/:id', async (requisicao, resposta) => {
+    request({
+        uri: 'http://localhost:8080/request/api/arquivoResumo/' + requisicao.params.id
+    }).pipe(resposta);
+});
+
+rotas.post('/empresario/:id/venda', async (requisicao, resposta) => {
+    const {idProd, idAgr, numParcela, precoProd} = requisicao.body;
+    request.post({
+        headers: { 'content-type': 'application/json' },
+        url: 'http://localhost:8080/request/api/empresario/'+requisicao.params.id+'/venda',
+        form: JSON.stringify({ idProd, idAgr, numParcela, precoProd })
+    });
 });
 
 module.exports = rotas;
