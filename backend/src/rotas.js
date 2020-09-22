6a7372f73964f832e65b8224a3167a7af329bf82
juliapcp <juliapcp2003@gmail.com>
2020-09-22 00:03:56 -0300

    rotas

--- backend/src/rotas.js
+++ backend/src/rotas.js
@@ -1,20 +1,21 @@
 const express = require('express');
 const request = require('request');
+const { json } = require('express');
 const rotas = express.Router();
 
-rotas.get('/empresarios', function (requisicao, resposta, next) {
+rotas.get('/empresarios', async (requisicao, resposta, next) => {
     request({
         uri: 'http://localhost:8080/request/api/mestre/empresarios'
     }).pipe(resposta);
 });
 
-rotas.get('/agricultores', function (requisicao, resposta, next) {
+rotas.get('/agricultores', async (requisicao, resposta, next) => {
     request({
         uri: 'http://localhost:8080/request/api/mestre/agricultores'
     }).pipe(resposta);
 });
 
-rotas.get('/infoMundo', function (requisicao, resposta, next) {
+rotas.get('/infoMundo', async (requisicao, resposta, next) => {
     request({
         uri: 'http://localhost:8080/request/api/mestre/infoMundo'
     }).pipe(resposta);
@@ -39,11 +40,14 @@ rotas.get('/arquivoResumo/:id', async (requisicao, resposta) => {
 });
 
 rotas.post('/empresario/:id/venda', async (requisicao, resposta) => {
-    const {idProd, idAgr, numParcela, precoProd} = requisicao.body;
+    const jsonVenda = resposta.json(requisicao.body);
     request.post({
-        headers: { 'content-type': 'application/json' },
+        headers: { 
+            'Content-Type': 'application/json',
+            'User-Agent': ''
+        },
         url: 'http://localhost:8080/request/api/empresario/'+requisicao.params.id+'/venda',
-        form: JSON.stringify({ idProd, idAgr, numParcela, precoProd })
+        form: jsonVenda
     });
 });
 
